56d9af7d46fdde66a22e151127b8569566682809
aviaIguazio <avia@iguazio.com>
2020-02-26 16:04:01 +0200

    updating updating client configuration

--- faces/client/config/app_conf.py
+++ faces/client/config/app_conf.py
@@ -10,16 +10,6 @@ class AppConf(object):
         config.read(self.conf_file)
 
         self.log_level = config['app']['log_level']
-        self.partition = config['app']['partition']
-
-        self.webapi_url = config['webapi']['url']
-        self.container = config['webapi']['container']
-        self.stream_name = config['webapi']['stream_name']
-        self.data_set_path = config['webapi']['dataset_path']
-        self.username = config['auth']['username']
-        self.password = config['auth']['password']
-        self.session_key = config['auth']['session_key']
-        self.nuclio_mount = config['nuclio']['mount']
         self.nuclio_url = config['nuclio']['url']
 
 
