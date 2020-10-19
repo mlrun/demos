339ca2cb5f8ffb1dd2a90310a39fe550a1cab832
Or Zilberman <zilbermanor@gmail.com>
2020-10-19 11:58:28 +0000

    Upgraded tf v2 model to efficientnet

--- horovod-pipe/src-tfv2/horovod-training.py
+++ horovod-pipe/src-tfv2/horovod-training.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import horovod.tensorflow.keras as hvd
 
 # to deprecate, use mlrun.mlutils.models and make this model a parameter instead:
-from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications import EfficientNetB7
 
 from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.models import Model
@@ -82,7 +82,7 @@ total_train = train_df.shape[0]
 total_validate = validate_df.shape[0]
 
 # load model
-model = VGG16(include_top=False, input_shape=IMAGE_SHAPE)
+model = EfficientNetB7(include_top=False, input_shape=IMAGE_SHAPE)
 
 # mark loaded layers as not trainable
 for layer in model.layers:
