c9db1d5aa9ea47651a482127f350f67cc39fc2e4
Edmond GZL <edmondg@iguazio.com>
2021-05-30 14:44:01 +0000

    Initial version

--- stock-analysis/functions/download_pretrained_model.py
+++ stock-analysis/functions/download_pretrained_model.py
@@ -3,9 +3,17 @@ import os
 
 
 def download():
-    model_location = 'https://iguazio-sample-data.s3.amazonaws.com/models/model.pt'
+    
+    url_prefix = os.environ.get('SAMPLE_DATA_SOURCE_URL_PREFIX', 'https://s3.wasabisys.com/iguazio/')
+    model_location = f'{url_prefix}data/stock-analysis/model.pt'
+
     saved_models_directory = os.path.join(os.path.abspath('../tests/'), 'models')
     # Create paths
     os.makedirs(saved_models_directory, exist_ok=1)
-    command = "wget -nc -P {} {}".format(saved_models_directory,model_location)
+    
+    if "http" in model_location:
+        command = "wget -nc -P {} {}".format(saved_models_directory,model_location)
+    else:
+        command = "cp {} {}".format(model_location, saved_models_directory)
+        
     os.system(command)
