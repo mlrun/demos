e9c6a12af47e91220cdedc7e59b3d3a8cacb1d29
yaron haviv <yaronh@iguazio.com>
2022-01-07 02:39:14 +0200

    add netops demo

---
+++ network-operations/README.md
@@ -0,0 +1,97 @@
+# NetOps Demo: Predictive Network Operations/Telemetry
+
+[Overview](#overview)&nbsp;| [Running the Demo](#demo-run)&nbsp;| [Demo Flow](#demo-flow)&nbsp;|  [Notebooks and Code](#notebooks-and-code)
+
+## Overview
+
+This demo demonstrates how to build an automated machine-learning (ML) pipeline for predicting network outages based on network-device telemetry, also known as Network Operations (NetOps).
+The demo implements both model training and inference, including model monitoring and concept-drift detection.
+The demo simulates telemetry network data for running the pipeline.
+
+The demo demonstrates how to
+
+- Manage MLRun projects.
+- Use GitHub as a source for functions to use in pipeline workflows.
+- Use MLRun logging to track results and artifacts.
+- Use MLRun's Feature Store for feature engineering.
+- Deploy a live-endpoints production pipeline.
+
+> **Note:** The demo applications are tested on the [Iguazio Data Science Platform](https://www.iguazio.com) ("the platform"), and use the platform's data store ("v3io").
+> Contact [Iguazio support](mailto:support@iguazio.com) to request a free trial of the platform.
+
+<a id="demo-run"></a>
+## Running the Demo
+
+<a id="demo-run-prerequisites"></a>
+### Prerequisites
+
+Before you begin, ensure that you have the following:
+
+- A [Kubernetes](https://kubernetes.io/) and [Nuclio](https://nuclio.io/).
+- An installation of MLRun with a running MLRun service and an MLRun dashboard.
+    See details in the [MLRun README](https://github.com/mlrun/mlrun).
+
+<a id="demo-execution-steps"></a>
+### Execution Steps
+
+Execute the following steps to run the demo:
+
+1. Fork the [mlrun/demos](https://github.com/mlrun/demos) Git repository to your GitHub account.
+
+2. In a client or notebook that is properly configured with MLRun, run the following code; replace `<your fork>` with the name of your mlrun/demos GitHub fork:
+    ```
+    mlrun project demos/network-operations/ -u git://github.com/<your fork>/demos/network-operations.git
+    ```
+
+3. Run the [**01-ingest.ipynb**](01-ingest.ipynb) notebook to create the feature sets and deploy the data generator and live ingestion endpoints.
+
+4. Open the [**02-fv-training.ipynb**](02-fv-training.ipynb) notebook and follow the instructions to create a Feature Vector, train a model and deploy it for live real-time predictions.
+
+<a id="demo-flow"></a>
+## Demo Flow
+
+The demo implements three main operations:
+
+- [**Feature Ingestion & Engineering**](#feature-creation) &mdash; Create a feature set for the network-device telemetry and deploy live ingestion endpoints.
+- [**Model Training**](#model-training) &mdash; Create a Feature Vector from the available features and retrieve a Dataset. Create a model from this dataset for network device failure prediction.
+- [**Model Deployment & Monitoring**](#model-Deployment-and-monitoring) &mdash; Deploy the generated model to a live endpoint and monitor it through grafana.
+
+<a id="feature-creation"></a>
+### Feature Creation
+
+In the feature creation stage we use MLRun's Feature Store to first define the features we are want to ingest and the operations we want to apply to them.  
+In this demo we create 3 unique feature sets:
+- **network-device metrics** &mdash; real-time network-device telemtry data such as cpu utilization, packet loss, latency, throughput. We will run real-time aggregations on top of these metrics with different time windows.
+- **static network-device data** &mdash; static device data such as his model and manufacturing country. We will one-hot-encode the categorical features to have them ready for model ingestion.
+- **network-device failure label indicator** &mdash; a stream representing the label responses. The Feature Store will later match them with the correct sample for dataset creation.
+
+
+<a id="model-training"></a>
+### Model Training
+
+In the model training stage we will use MLRun's Feature Store to define the Feature Vector we want to use for our model from the features we created and the labels we ingest.  We will create a dataset from the feature vector and feed it to our [sklearn classifier training function](https://github.com/mlrun/functions/blob/master/sklearn_classifier/sklearn_classifier.ipynb) from the [MLRun marketplace functions hub](https://github.com/mlrun/functions) and create a network-device failure prediction model.
+
+
+<a id="model-Deployment-and-monitoring"></a>
+### Model Deployment & Monitoring
+
+In this stage we will use MLRun's model server to deploy our trained model to production.  We will use the `EnrichmentModelRouter` to retrieve the online feature vector automatically upon receiving a network-device id and predict device failures.  
+
+We will then be able to use MLRun's Model Montoring through Grafana to see our model performance, feature analytics and drift metrics.
+
+
+<a id="notebooks-and-code"></a>
+## Notebooks and Code
+
+<a id="notebooks"></a>
+### Notebooks
+
+- [**01-ingest.ipynb**](01-ingest.ipynb) &mdash; the 1st demo step notebook. including project setup, genetaor deployment, feature sets creation and deployment.
+- [**02-training.ipynb**](02-fv-training.ipynb) &mdash; the 2nd demo step notebook. including feature vecto creation, dataset creation, model training, deployment and testing.
+- [**src/generator.py**](src/generator.py) &mdash; a nuclio function to generate live network-device telemetry and publish it to a v3io stream.
+
+<a id="project-cfg-files"></a>
+### Project-Configuration Files
+
+- [**src/metric_configurations.yaml**](src/metric_configurations.yaml) &mdash; a data-generator configurations file. defines the metrics for the demo's generator network-device telemetry data.
+
---
+++ network-operations/src/generator.py
@@ -0,0 +1,188 @@
+import os
+import yaml
+import pandas as pd
+import datetime
+import json
+import mlrun
+from typing import Dict
+import copy
+import random
+import mlrun.feature_store as fstore
+
+# Data generator
+from v3io_generator import metrics_generator, deployment_generator
+
+
+def _create_deployment(country, models, project=None):
+    print("creating deployment")
+    # Create meta-data factory
+    dep_gen = deployment_generator.deployment_generator()
+    faker = dep_gen.get_faker()
+
+    # Design meta-data
+    num_devices = 20
+    dep_gen.add_level("device", number=num_devices, level_type=faker.msisdn)
+
+    # Create meta-data
+    deployment_df = dep_gen.generate_deployment()
+
+    # Add static metrics
+    static_df = copy.copy(deployment_df)
+    static_df["country"] = [random.choice(country) for i in range(num_devices)]
+    static_df["model"] = [random.choice(models) for i in range(num_devices)]
+
+    # Add stub data
+    deployment_df["cpu_utilization"] = 70
+    deployment_df["latency"] = 0
+    deployment_df["packet_loss"] = 0
+    deployment_df["throughput"] = 290
+
+    if project:
+        # save the simulated dataset for future use
+        project.log_dataset("deployment", df=deployment_df, format="parquet")
+        project.log_dataset("static", df=static_df, format="parquet")
+
+    return deployment_df, static_df
+
+
+def get_or_create_deployment(country, models, project=None, create_new=False):
+    if project and not create_new:
+        try:
+            static_df = mlrun.get_dataitem(project.get_artifact_uri("static")).as_df()
+            deployment_df = mlrun.get_dataitem(
+                project.get_artifact_uri("deployment")
+            ).as_df()
+            return deployment_df.reset_index(), static_df
+        except:
+            pass
+
+    # Create deployment
+    return _create_deployment(country, models, project)
+
+
+def get_data_from_sample(context, data: Dict, as_df: bool = False) -> Dict:
+    deployment_levels = (
+        context.deployment_levels
+        if context and hasattr(context, "deployment_levels")
+        else ["device"]
+    )
+    label_col_indicator = (
+        context.label_col_indicator
+        if context and hasattr(context, "label_col_indicator")
+        else "is_error"
+    )
+    base_columns = deployment_levels + ["timestamp"]
+    metrics = {k: v for k, v in data.items() if label_col_indicator not in k}
+    labels = {
+        k: v for k, v in data.items() if label_col_indicator in k or k in base_columns
+    }
+
+    if as_df:
+        metrics = pd.DataFrame.from_dict(metrics)
+        labels = pd.DataFrame.from_dict(labels)
+
+    return metrics, labels
+
+
+def get_sample(
+    metrics_configuration: dict,
+    as_df: bool = True,
+    project=None,
+    ticks=5,
+    create_new=False,
+):
+    deployment_df, static_df = get_or_create_deployment(
+        metrics_configuration["country"],
+        metrics_configuration["models"],
+        project=project,
+        create_new=create_new,
+    )
+    initial_timestamp = int(
+        os.getenv(
+            "initial_timestamp",
+            (datetime.datetime.now() - datetime.timedelta(days=1)).timestamp(),
+        )
+    )
+    met_gen = metrics_generator.Generator_df(
+        metrics_configuration,
+        user_hierarchy=deployment_df,
+        initial_timestamp=initial_timestamp,
+    )
+
+    generator = met_gen.generate(as_df=True)
+    for i in range(100):
+        sample = next(generator)
+    metrics_df, labels_df = get_data_from_sample(None, sample, as_df)
+    for i in range(ticks):
+        sample = next(generator)
+        metrics2_df, labels2_df = get_data_from_sample(None, sample, as_df)
+        metrics_df = metrics_df.append(metrics2_df)
+        labels_df = labels_df.append(labels2_df)
+    return metrics_df, labels_df, static_df
+
+
+def init_context(context):
+
+    # Get metrics configuration
+    project = mlrun.get_run_db().get_project(mlrun.mlconf.default_project)
+    params = project.params
+    config = mlrun.get_dataitem(params["metrics_configuration_uri"]).get()
+    metrics_configuration = yaml.safe_load(config)
+
+    # Generate or create deployment
+    deployment_df, static_deployment = get_or_create_deployment(
+        metrics_configuration["country"],
+        metrics_configuration["models"],
+        project=project,
+    )
+
+    static_set = fstore.get_feature_set("static")
+    fstore.ingest(static_set, static_deployment)
+
+    setattr(context, "label_col_indicator", "error")
+    setattr(context, "deployment_levels", ["device"])
+
+    # Create metrics generator
+    initial_timestamp = int(
+        os.getenv(
+            "initial_timestamp",
+            (datetime.datetime.now() - datetime.timedelta(days=1)).timestamp(),
+        )
+    )
+    met_gen = metrics_generator.Generator_df(
+        metrics_configuration,
+        user_hierarchy=deployment_df,
+        initial_timestamp=initial_timestamp,
+    )
+    generator = met_gen.generate(as_df=True)
+    setattr(context, "metrics_generator", generator)
+
+    # Metrics pusher
+    device_metrics_pusher = mlrun.datastore.get_stream_pusher(
+        params["device_metrics_stream"]
+    )
+    setattr(context, "device_metrics_pusher", device_metrics_pusher)
+
+    # Labels pusher
+    device_labels_pusher = mlrun.datastore.get_stream_pusher(
+        params["device_labels_stream"]
+    )
+    setattr(context, "device_labels_pusher", device_labels_pusher)
+
+
+def handler(context, event):
+    for i in range(10):
+        # Generate sample from all devices in the network
+        device_metrics = json.loads(
+            next(context.metrics_generator)
+            .reset_index()
+            .to_json(orient="records", date_unit="s")
+        )
+        for metric in device_metrics:
+            # Split the data to features and labels
+            metrics, labels = get_data_from_sample(context, metric)
+
+            # Push the data to the appropriate streams
+            context.device_metrics_pusher.push(metrics)
+            context.device_labels_pusher.push(labels)
+    return metrics, labels
