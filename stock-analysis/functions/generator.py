38bff395d5e225b5da767afb8ef7e590830b55b2
Avi Asulin <avia@iguazio.com>
2022-06-07 17:07:18 +0300

    update ingest

---
+++ stock-analysis/functions/generator.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+
+import pandas
+import yahoo_fin.stock_info as si
+import datetime
+import yfinance as yf
+import pandas as pd
+import json
+import time
+
+
+class IGenerator(ABC):
+
+    @abstractmethod
+    def generate(self):
+        pass
+
+    def create_file_name(self):
+        file_name = time.strftime("%Y%m%d-%H%M%S") + '-' + self.__class__.__name__ + '.csv'
+        return file_name
+
+
+class StocksGenerator(IGenerator, ABC):
+
+    def generate(self, number_of_stocks, start_delta, end_delta, interval, path):
+        """
+        event: dict with the following keys:
+        start_delta - start collecting the data days back
+        end_delta - collect data untill days back
+        interval - interval of collected stocks data
+        n_stocks - how many stocks to collect
+        """
+        # getting stock names
+        tickers = si.tickers_sp500()[:number_of_stocks]
+        # time deltas to scrape data
+        start = datetime.datetime.now() - datetime.timedelta(start_delta)
+        end = datetime.datetime.now() - datetime.timedelta(end_delta)
+        interval = interval
+
+        # collecting data from yfinance
+        return_list = []
+        for ticker in tickers:
+            hist = yf.Ticker(ticker).history(start=start, end=end, interval=interval)
+            hist['ticker'] = ticker
+            hist['ticker2onehot'] = ticker
+            return_list.append(hist)
+
+        # some data manipulations
+        df: pandas.DataFrame = pd.concat(return_list).reset_index().drop(axis=1, columns=['Dividends', 'Stock Splits'])
+        print(f"downloaded {len(tickers)} stocks data with size of {df.shape}")
+        df['Datetime'] = df['Datetime'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
+        file_name = self.create_file_name()
+        df.to_csv(file_name, index=False)
+        return json.loads(df.to_json(orient='records'))
+
+
+class NewsGenerator(IGenerator):
+
+    def generate(self):
+        return
---
+++ stock-analysis/tests/test_generator.py
@@ -0,0 +1,21 @@
+from functions.generator import *
+from mlrun import code_to_function
+
+
+def test_stocks_generator():
+    gen = StocksGenerator()
+    print(gen.generate(number_of_stocks=4, start_delta=7, end_delta=3, interval='5m', path='myfile.csv'))
+
+
+def test_stocks_generator_job():
+    fn = code_to_function(filename="../functions/generator.py",
+                          handler="StocksGenerator::generate",
+                          kind='job'
+                          )
+
+    fn.run(params={'start_delta': 3,
+                   'end_delta': 0,
+                   'interval': '5m',
+                   'number_of_stocks': 4,
+                   'path': 'mycsv.csv'},
+           local=True)
