626f06e8058134dbcfd886c59c2411faae5a853f
daniels290813 <daniels@iguazio.com>
2022-11-10 14:57:07 +0000

    [NLP] redis support

--- news-article-nlp/nlp_transformations.py
+++ news-article-nlp/nlp_transformations.py
@@ -17,6 +17,8 @@ from transformers import pipeline
 from keybert import KeyBERT
 import json
 import storey
+import redis
+import os
 
 def fetch_article(event):
     """
@@ -105,3 +107,14 @@ def kv_format(event):
             event.body[k] = json.dumps(v)
     
     return event
+
+def write_to_redis(event):
+    redis_url = os.environ.get('MLRUN_REDIS_URL')
+    redis_pass = os.environ.get('MLRUN_REDIS_PASS')
+    if redis_url:
+        r = redis.Redis(host = redis_url[:redis_url.index(":")],
+                        port = redis_url[redis_url.index(":")+1:])
+        
+        r.auth(redis_pass)
+        event_url = event.body.pop('url')
+        r.hmset(event_url, event.body)
